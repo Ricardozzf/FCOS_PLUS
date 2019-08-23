fix bug normalization

maskrcnn_benchmark/modeling/backbone/resnet.py
@@ -291,6 +291,9 @@ class Bottleneck(nn.Module):
 
         self.globalAvgPool = nn.AdaptiveAvgPool2d((1,1))
 
+        self.fc1 = nn.Linear(in_features = out_channels, out_features = round(out_channels / 8))
+        self.fc2 = nn.Linear(in_features = round(out_channels / 8), out_features = out_channels)
+
         for l in [self.conv1, self.conv2, self.conv3,]:
             nn.init.kaiming_uniform_(l.weight, a=1)
 
@@ -315,10 +318,6 @@ class Bottleneck(nn.Module):
         w_1 = self.globalAvgPool(out1)
         w_2 = self.globalAvgPool(out2)
         w_3 = self.globalAvgPool(out3)
-
-        w_1 = w_1 / w_1.max(1)[0].unsqueeze(1)
-        w_2 = w_2 / w_2.max(1)[0].unsqueeze(1)
-        w_3 = w_3 / w_3.max(1)[0].unsqueeze(1)
 
         w_1 = w_1 / (w_1 + w_2 + w_3)
         w_2 = w_2 / (w_1 + w_2 + w_3)